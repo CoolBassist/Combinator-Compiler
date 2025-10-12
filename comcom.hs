1625f85f036de86fcc9a6b33c47ac471e530940f
Coolbassist <erreyjoe@gmail.com>
2025-10-12 12:36:42 +0100

    1. Improved naming and print output

--- comcom.hs
+++ comcom.hs
@@ -1,17 +1,21 @@
--- \x.x -> i
--- \x.M -> k M   if x is free in M
+-- \x.x  -> i
+-- \x.M  -> k M   if x is free in M
 -- \x.QP -> S (\x.Q) (\x.P)
 
 data Expression = Lam Char Expression | Var Char | App Expression Expression | S | K | I deriving (Show)
 
-comp :: Expression -> Expression
-comp (Lam x (Var y)) = if x == y then I else App K (Var y)
-comp (Lam x (App y z)) = App (App S (comp (Lam x y))) (comp (Lam x z))
-comp (Lam x (Lam y z)) = comp (Lam x (comp (Lam y z)))
-comp (Lam x y) = App K y -- For lone combinators
+compile :: Expression -> Expression
+compile (Lam x (Var y)) = if x == y then I else App K (Var y)
+compile (Lam x (App y z)) = App (App S (compile (Lam x y))) (compile (Lam x z))
+compile (Lam x (Lam y z)) = compile (Lam x (compile (Lam y z)))
+compile (Lam x y) = App K y -- For lone combinators
 
 prettyPrint :: Expression -> String
-prettyPrint (App x y) = "(" ++ prettyPrint x ++ prettyPrint y ++ ")"
+prettyPrint (App x (App y z)) = prettyPrint x ++ "(" ++ prettyPrint (App y z) ++ ")"
+prettyPrint (App x y) = prettyPrint x ++ prettyPrint y
 prettyPrint S = "S"
 prettyPrint K = "K"
 prettyPrint I = "I"
+
+-- prettyPrint (comp (Lam 'x' (Lam 'y' (App (Var 'x') (Var 'y')))))
+-- "S(S(KS)(S(KK)I))(KI)"
